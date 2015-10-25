31df04544ead953d202647d873376eedef51d262
Matthew Dickens <matthew@matthew-dickens.com>
2015-10-25 15:58:02 -0500

    Now supports every single file type

--- downloadsOrganizer.py
+++ downloadsOrganizer.py
@@ -12,7 +12,7 @@ import sys
 import time
 import re
 FILETYPE = ['png','PNG','ppt','PPT','jpg','jpeg','JPG','gif','GIF','doc','DOC','pdf','PDF','exe','zip','ZIP','msi','MSI','txt','TXT','md']
-REGEX = '^.*\.(png|PNG|ppt|PPT|jpg|jpeg|JPG|gif|GIF|doc|DOC|pdf|PDF|exe|zip|ZIP|msi|MSI|txt|TXT|md)$'
+REGEX = '^.*\.(.*)$'
 PATH_DOWNLOADS = "c:\\Users\\"+os.getenv("USERNAME")+"\\Downloads\\"
 FILES = os.listdir(PATH_DOWNLOADS)
 
@@ -26,16 +26,10 @@ def main():
             directory = PATH_DOWNLOADS + match.group(1)
             if not os.path.exists(directory):
                 file_path_creation(directory)
-            '''
-            ISSUE #1 FIXED
-            Silly mistake from the previous version having the"if not" statement
-            be the only one that dictates the file can move but in both cases if the
-            filepath exists or not it will always move to its respective directory.
-            '''
-            print 'Moving -> ' , _file_
+            print ('Moving -> ' + _file_)
             os.rename(PATH_DOWNLOADS+match.group(0),PATH_DOWNLOADS+match.group(1)+"\\"+match.group(0))
         else:
-            print 'Not a file, must be a directory ->' , _file_
+            print ('Not a file, must be a directory ->' + _file_)
     printEnv()
 
 def printEnv():
@@ -45,9 +39,9 @@ def printEnv():
     print FILES
 def file_path_creation(path):
     try:
-        print 'Creating non existant directory' + os.path.splitext(path)[-1].lower()
+        print ('Creating non existant directory' + os.path.splitext(path)[-1].lower())
         os.makedirs(path)
-        print 'Directory successfully created'
+        print ('Directory successfully created')
     except OSError as exception:
         if exception.errno != errno.EEXIST:
             raise
