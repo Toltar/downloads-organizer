56aef18bcbf8639090238578ff0f7799c9c5483b
Matthew Dickens <matthew@matthew-dickens.com>
2015-10-25 03:04:46 -0500

    Now it creates directories and moves files

--- downloadsOrganizer.py
+++ downloadsOrganizer.py
@@ -3,8 +3,11 @@ Author: Matthew Dickens
 Email: matthew@matthew-dickens.com
 GitHub: github.com/toltar
 VCS: git
+
 '''
 import os
+import errno
+import shutil
 import sys
 import time
 import re
@@ -20,11 +23,12 @@ def main():
     for _file_ in FILES:
         match = pattern.match(_file_)
         if match:
-            extension = os.path.splitext(PATH_DOWNLOADS+_file_)[-1].lower()
-            if extension in ".pdf":
-                print extension
+            directory = PATH_DOWNLOADS + match.group(1)
+            if not os.path.exists(directory):
+                file_path_creation(directory)
+                os.rename(PATH_DOWNLOADS+match.group(0),PATH_DOWNLOADS+match.group(1)+"\\"+match.group(0))
         else:
-            print 'Filetype doesn\'t match'
+            print ''
     printEnv()
 
 def printEnv():
@@ -32,7 +36,14 @@ def printEnv():
     print REGEX
     print PATH_DOWNLOADS
     print FILES
-
+def file_path_creation(path):
+    try:
+        print 'Creating non existant directory' + os.path.splitext(path)[-1].lower()
+        os.makedirs(path)
+        print 'Directory successfully created'
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
 
 #Call Main Process
 main()
